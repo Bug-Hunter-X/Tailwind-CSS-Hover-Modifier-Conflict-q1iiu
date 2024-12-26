dbec78c130af32909f3c68babad84d36dc6e87f0
BugHunter-X <askzenai@gmail.com>
2024-12-26 02:04:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// Example of an incorrect use of Tailwind's hover modifier that leads to unexpected behavior.
+<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded hover:shadow-lg">
+  Hover me!
+</button>
+```
